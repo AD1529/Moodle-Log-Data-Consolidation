4117a4966139551ce203950297e6869789d3eff9
Daniela Rotelli <65086299+AD1529@users.noreply.github.com>
2023-05-15 11:11:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import src.algorithms.integrating as it
 import src.algorithms.cleaning as cl
+import src.algorithms.filtering as fl
 import src.algorithms.transforming as tr
 from pandas import DataFrame
 
@@ -100,7 +101,7 @@ if __name__ == '__main__':
                                deleted_users=DELETED_USERS_PATH)
 
     # remove useless data from the entire dataset
-    # df = cl.clean_dataset_records(df)
+    # df = fl.filter_dataset_records(df)
 
     # you can save the dataset for further analysis
     df.to_csv('src/datasets/consolidated_df.csv')
